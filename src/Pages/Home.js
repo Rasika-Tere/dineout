d61a34de1fc75c517773abdce6d62d5c9c680bcd
Rasika-Tere <76546357+Rasika-Tere@users.noreply.github.com>
2021-09-15 17:28:21 +0530

    folder and components

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,10 @@ import Container from '@material-ui/core/Container';
 import DatePickers from '../Components/DateTimePicker';
 import BasicButtonGroup from '../Components/GroupButton';
 import MaterialUIPickers from '../Components/datetime';
-
+import TextField from '@material-ui/core/TextField';
+import SimplePaper from '../Components/paper';
+import SimpleAccordion from '../Components/accordian';
+import BasicImageList from '../Components/Layout/ImageList';
 
 export default function Home() {
   return (
@@ -17,8 +20,12 @@ export default function Home() {
           To get started, edit <code>src/App.js</code>, save, and reload this page.
         </p>
         < DatePickers></DatePickers>
+        <TextField/> this is me
+        <SimplePaper/>
         < BasicButtonGroup />
         < MaterialUIPickers />
+        <SimpleAccordion/>
+        <BasicImageList/>
        
 
       </Container>
