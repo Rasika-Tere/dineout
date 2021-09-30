83a1d7eb4c08131099fc194705824a3f09996033
Rasika-Tere <76546357+Rasika-Tere@users.noreply.github.com>
2021-10-01 01:05:22 +0530

    no luck

--- src/Pages/Grid.js
+++ src/Pages/Grid.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
-import FinalBookGrid from '../Components/FinalBookPage/FinalBookGrid';
-
-
-
 
 export default function Grid() {
   return (
