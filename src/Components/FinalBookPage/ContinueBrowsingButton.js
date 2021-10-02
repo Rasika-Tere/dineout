9eeef0dbbd5010dc035d1dc1e2e254451b52af8f
Rasika-Tere <76546357+Rasika-Tere@users.noreply.github.com>
2021-10-02 11:11:49 +0530

    nope

--- src/Components/FinalBookPage/ContinueBrowsingButton.js
+++ src/Components/FinalBookPage/ContinueBrowsingButton.js
@@ -4,28 +4,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import {Link, Router} from "react-router-dom";
+import { Link, Router } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(5),
+ 
     },
   },
 }));
 
 export default function ContinueBrowsingButton() {
   const classes = useStyles();
-
+  
+  
   return (
     <div className={classes.root}>
-     
-     <Link to={process.env.PUBLIC_URL + '/home'}>
-      <Button variant="contained" color="Secondary"  href="">
-        Continue Browsing 
-      </Button>
+      <Link to={process.env.PUBLIC_URL + '/home'}>
+        <Button variant="contained" color="Secondary" href="">
+          Continue Browsing
+        </Button>
       </Link>
-   
+
     </div>
   );
 }
