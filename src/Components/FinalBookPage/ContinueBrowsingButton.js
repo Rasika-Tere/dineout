436df4ce8c21115e6af19924bcb03be78ea10915
Rasika-Tere <76546357+Rasika-Tere@users.noreply.github.com>
2021-10-08 15:18:19 +0530

    styles installed

--- src/Components/FinalBookPage/ContinueBrowsingButton.js
+++ src/Components/FinalBookPage/ContinueBrowsingButton.js
@@ -10,12 +10,17 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
+      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      border: 0,
+      borderRadius: 30,
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
     }
 
   },
-  Color: {
-    
-  }
+  
 
 }));
 
